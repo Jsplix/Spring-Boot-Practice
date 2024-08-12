054074da16ed533580186cfa3256b37f363c6909
Jsplix <jkjin112@gmail.com>
2024-08-13 01:02:17 +0900

    feat: user request dto 구현

--- src/main/java/com/practice/blog/bounded_context/user/dto/UserRequestDto.java
+++ src/main/java/com/practice/blog/bounded_context/user/dto/UserRequestDto.java
@@ -1,2 +1,19 @@
-package com.practice.blog.bounded_context.user.dto;public class UserRequestDto {
+package com.practice.blog.bounded_context.user.dto;
+
+import com.practice.blog.bounded_context.user.entity.User;
+
+public record UserRequestDto(
+        String username,
+        String nickname,
+        String password,
+        String email
+) {
+    public User toEntity() {
+        return User.builder()
+                .username(username)
+                .nickname(nickname)
+                .password(password)
+                .email(email)
+                .build();
+    }
 }
